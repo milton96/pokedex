cecd078c29408579e5a5bfbd5be489caf1940ff8
milton bravo <miltonbravo96@gmail.com>
2022-06-27 22:54:16 -0500

    toolbar basica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AyudaComponent } from './components/ayuda/ayuda.component';
 import { IndexComponent } from './components/index/index.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, AyudaComponent, IndexComponent, PokemonComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    AyudaComponent,
+    IndexComponent,
+    PokemonComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
