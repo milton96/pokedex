7e388e7e9519d2351bc8d35a0f3b53ad9e126d04
milton bravo <miltonbravo96@gmail.com>
2022-06-27 21:29:40 -0500

    llamada a api desde servicio

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Pokemon } from 'src/app/interfaces/pokemon';
+
 import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
+  public pokemon: Pokemon[] = [];
 
-  constructor(
-    private pokemonService: PokemonService
-  ) { }
+  constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-    this.pokemonService.getListPokemon();
+    this.getListaPokemon();
   }
 
+  /**
+   * getListaPokemon
+   */
+  public getListaPokemon(): void {
+    this.pokemonService.getListaPokemon().subscribe((response) => {
+      this.pokemon = response['results'] as Pokemon[];
+    });
+  }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,21 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { LogUtil } from '../utils/log.util';
+import { environment } from '../../environments/environment';
+import { Pagination } from '../interfaces/pagination';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  private baseUri: string = environment.baseUri;
 
-  constructor(
-    private http: HttpClient,
-    private log: LogUtil
-  ) { }
+  constructor(private http: HttpClient, private log: LogUtil) {}
 
   /**
    * getListPokemon
    */
-  public getListPokemon(): void {
-    this.log.log("Obtener lista de Pokémon");
+  public getListaPokemon(): Observable<Pagination> {
+    this.log.info('Obteniendo lista de Pokémon');
+    return this.http
+      .get<Pagination>(`${this.baseUri}/pokemon/?limit=6`)
+      .pipe(catchError(this.handleError<Pagination>('getListaPokemon', {} as Pagination)));
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log.error(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
