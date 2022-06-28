32ee7b2566bcd7fc6f8877b8b9ecb8b7f32a474b
milton bravo <miltonbravo96@gmail.com>
2022-06-27 20:05:11 -0500

    checar entorno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pokedex';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    if (isDevMode()) {
+      console.info('dev mode');
+    } else {
+      console.info('production mode');
+    }
+  }
 }
